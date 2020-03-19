4b80e763ef34cd5c289eb3997e24bd79feb1acfa
braisv <brais.vidal.rodriguez@gmail.com>
2020-03-19 13:34:12 +0100

    signup view

---
+++ src/views/Signup.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Sign Up</h1>
+        <form>
+            <input type="email" v-model="email">
+            <input type="password" v-model="password">
+            <input type="password" v-model="password2">
+            <button type="submit">Sign Up</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Signup',
+    data() {
+        return {
+            email: '',
+            password: '',
+            password2: ''
+        }
+    }
+}
+</script>
