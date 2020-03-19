775ae6801b890e82b08af04c121e92f830c331b8
braisv <brais.vidal.rodriguez@gmail.com>
2020-03-19 14:36:52 +0100

    signup functions

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+const firebase = require("firebase/app");
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: '',
+    error: ''
   },
   mutations: {
+    setUser(state, payload) {
+      state.user = payload
+    },
+    setError(state, payload) {
+      state.error = payload
+    }
   },
   actions: {
+    createUser({ commit }, payload) {
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+      .then( res => {
+        commit('setUser', { email: res.user.email, password: res.user.password, id: res.user.uid })
+      })
+      .catch( err => {
+        console.log(err.message)
+        commit('setError', err.message)
+      })
+    }
   },
   modules: {
   }
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
         <h1>Sign Up</h1>
-        <form>
+        <form @submit.prevent="createUser({ email: email, password: password })">
             <input type="email" v-model="email">
             <input type="password" v-model="password">
             <input type="password" v-model="password2">
             <button type="submit">Sign Up</button>
         </form>
+        {{error}}
     </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
+
+console.log("API KEY: ", process.env.FIREBASE_KEY)
+console.log("APP ID: ", process.env.FIREBASE_ID)
+
 export default {
     name: 'Signup',
     data() {
@@ -19,6 +25,12 @@ export default {
             password: '',
             password2: ''
         }
+    },
+    computed: {
+        ...mapState(['error'])
+    },
+    methods: {
+        ...mapActions(['createUser'])
     }
 }
 </script>
