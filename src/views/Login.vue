fd1ee7f1596f4225c26f5ff1495953e5cdd1b363
braisv <brais.vidal.rodriguez@gmail.com>
2020-03-25 13:19:33 +0100

    vuelidate login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,64 @@
 <template>
-    <div>
-        <h1>Log In</h1>
-        <form @submit.prevent="login({ email, password })">
-            <input type="email" v-model="$v.email.$model" class="form-control my-2">
-            <input type="password" v-model="$v.password.$model" class="form-control my-2">
-            <button type="submit" class="btn btn-info">Submit</button>
-        </form>
-        {{ error }}
-    </div>
+  <div>
+    <h1>Log In</h1>
+    <form
+      @submit.prevent="
+        login({ email: $v.email.$model, password: $v.password.$model })
+      "
+    >
+      <input
+        type="email"
+        v-model="$v.email.$model"
+        class="form-control my-2"
+        placeholder="E-mail"
+      />
+      <small class="text-danger d-block" v-if="!$v.email.required"
+        >Required Field</small
+      >
+      <small class="text-danger d-block" v-if="!$v.email.email"
+        >Invalid Email</small
+      >
+      <input
+        type="password"
+        v-model="$v.password.$model"
+        class="form-control my-2"
+        placeholder="Password"
+      />
+      <small class="text-danger d-block" v-if="!$v.password.required"
+        >Required Field</small
+      >
+      <small class="text-danger d-block" v-if="!$v.password.minLength"
+        >Minimum 6 characters</small
+      >
+      <button type="submit" class="btn btn-info mt-3" :disabled="$v.$invalid">
+        Submit
+      </button>
+    </form>
+    {{ error }}
+  </div>
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
+import { mapActions, mapState } from "vuex";
 import { required, minLength, email } from "vuelidate/lib/validators";
 
 export default {
-    name: 'Login',
-    data() {
-        return {
-            email: '',
-            password: ''
-        }
-    },
-    methods: {
-        ...mapActions(['login'])
-    },
-    computed: {
-        ...mapState(['error'])
-    },
-    validations: {
-        email: { required, email },
-        password: { required, minLength: minLength(6) }
-    }
-}
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    ...mapActions(["login"])
+  },
+  computed: {
+    ...mapState(["error"])
+  },
+  validations: {
+    email: { required, email },
+    password: { required, minLength: minLength(6) }
+  }
+};
 </script>
